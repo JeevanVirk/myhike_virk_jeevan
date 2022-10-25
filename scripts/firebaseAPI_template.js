6e6e043714c038c5621618322ec0fd83674e9b41
Jeevan Virk <jvirk37@my.bcit.ca>
2022-10-25 12:38:38 -0700

    demo8 additions

--- scripts/firebaseAPI_template.js
+++ scripts/firebaseAPI_template.js
@@ -2,12 +2,19 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyDWrBZMNi3MEv4ZiyEcLSG3IbhXbmDFiZY",
+
+  authDomain: "comp1800-2022-32bd2.firebaseapp.com",
+
+  projectId: "comp1800-2022-32bd2",
+
+  storageBucket: "comp1800-2022-32bd2.appspot.com",
+
+  messagingSenderId: "1078757206",
+
+  appId: "1:1078757206:web:a4fb61984ecbc6c5e6b8f7",
+
+  measurementId: "G-0HYLF2BT18"
 };
 
 //--------------------------------------------
